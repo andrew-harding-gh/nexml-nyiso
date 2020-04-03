26bdf67089082fac11c42bb5e10cd3bc4452ea9c
Nick Browning <nbrownin@emich.edu>
2020-04-03 01:44:18 -0400

    Update project structure.

--- nexml_nyiso/bin/nyiso_weather_data.py
+++ nexml_nyiso/bin/nyiso_weather_data.py
@@ -11,18 +11,20 @@ def download_files():
         for month in range(1, 13):
             target = url.format(year=year, month=month)
             f = requests.get(target)
-            open('data/raw_nyiso_weather_forecast/' + target.split('/')[-1], 'wb').write(f.content)
-            print('Downloaded from: {location}'.format(location=target))
+            if f.status_code == 200:
+                open('../raw_data/nyiso_weather_forecast/' + target.split('/')[-1], 'wb').write(f.content)
+                print('Downloaded from: {location}'.format(location=target))
 
 
 def create_csv():
     forecast_data = []
-    file_names = glob.glob('./data/raw_nyiso_weather_forecast/*.zip')
+    file_names = glob.glob('../raw_data/nyiso_weather_forecast/*.zip')
     for file in file_names:
         with ZipFile(file) as myzip:
             for csv_file in myzip.filelist:
                 with myzip.open(csv_file.filename) as csv:
                     df = pd.read_csv(csv)
+                    # Some of the CSVs have a 10th column filled with NaN
                     df = df[[
                         'Forecast Date',
                         'Vintage Date',
@@ -34,7 +36,7 @@ def create_csv():
                         'Min Wet Bulb']]
                     df['source_file_name'] = csv_file.filename
                     forecast_data.append(df)
-    pd.concat(forecast_data).to_csv('data/nyiso_weather_forecast.csv', index=False)
+    pd.concat(forecast_data).to_csv('../data/nyiso_weather_forecast.csv', index=False)
 
 
 if __name__ == "__main__":
