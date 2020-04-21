800efa361f224c1890e3c987c548b792c292ad72
Andrew Harding <41498272+the-great-shazbot@users.noreply.github.com>
2020-04-21 13:14:21 -0400

    Update elec expl (#15)

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -5,6 +5,7 @@ START_DATE = datetime.datetime(2005, 2, 1)
 END_DATE = datetime.datetime(2020, 3, 30)
 WEATHER_DATA_PATH = '../data/central_park_weather.csv'
 PAL_DATA_PATH = '../data/nyiso_pal_master.csv'
+ISOLF_DATA_PATH = '../data/nyiso_isolf_master.csv'
 
 
 def date_filter(df):
@@ -42,6 +43,18 @@ def load_pal_data():
         'pal_min',
         'pal_max',
         'pal_mean',
+    ]]
+    df = df.set_index('Time Stamp').sort_index()
+    return date_filter(df)
+
+def load_isolf_data():
+    df = pd.read_csv(ISOLF_DATA_PATH)
+    df['Time Stamp'] = pd.to_datetime(df['Time Stamp'])
+    df = df[[
+        'Time Stamp',
+        'isolf_min',
+        'isolf_max',
+        'isolf_mean',
     ]]
     df = df.set_index('Time Stamp').sort_index()
     return date_filter(df)
