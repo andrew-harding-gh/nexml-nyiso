f5766776df5f760fd05d03ce7a2a435dc61557e9
Rayaan Colvin <rcolvin4@emich.edu>
2020-05-11 16:47:03 -0400

    Code cleanup for population script

--- scripts/population.py
+++ scripts/population.py
@@ -2,15 +2,17 @@
 # NYC population data from yearly NYC population data
 import pandas as pd
 
+def daily_population_csv(yearly_df):
+    date_df = pd.date_range(start='2/1/2005', end='3/30/2020').to_frame(index=False, name='date')
+    date_df['year'] = date_df.date.dt.year
+    date_df['day_of_year'] = date_df.date.dt.dayofyear
+    daily_population_df = date_df.join(yearly_df.set_index('year'), on='year')
+    daily_population_df['daily_population_difference'] = daily_population_df.apply(lambda x: (x.day_of_year/daily_population_df[daily_population_df['year'] == x.year].shape[0]) * (yearly_df[yearly_df['year'] == x.year + 1].iloc[0].population - x.population), axis=1)
+    daily_population_df['total_daily_population'] = daily_population_df['daily_population_difference'] + daily_population_df['population']
+    print('bp')
+    return daily_population_df.to_csv('daily_population.csv', sep=',', index=False)
 
+if __name__ == "__main__":
+    test = daily_population_csv(pd.read_csv('../raw_data/nyc_population.csv', sep='\t'))
+    print('bp')
 
-# yearly_population_df = pd.read_csv('../raw_data/nyc_population.csv', sep='\t', names=['year', 'population', 'yearly growth rate'])
-
-df = pd.read_csv('../raw_data/nyc_population.csv', sep='\t')
-df2 = pd.date_range(start='2/1/2005', end='3/30/2020').to_frame(index=False, name='date')
-df2['year'] = df2.date.dt.year
-df2['day_of_year'] = df2.date.dt.dayofyear
-df3 = df2.join(df.set_index('year'), on='year')
-df3['daily_population_change'] = df3.apply(lambda x: (x.day_of_year/df3[df3['year'] == x.year].shape[0]) * (df[df['year'] == x.year + 1].iloc[0].population - x.population), axis=1)
-df3['total_daily_population'] = df3['daily_population_change'] + df3['population']
-print('bp')
