26e3df51c6127f5bb5f50479d879be18a2e1f45e
Rayaan Colvin <rcolvin4@emich.edu>
2020-05-11 17:01:01 -0400

    commting the csv created by population script

--- scripts/population.py
+++ scripts/population.py
@@ -9,10 +9,7 @@ def daily_population_csv(yearly_df):
     daily_population_df = date_df.join(yearly_df.set_index('year'), on='year')
     daily_population_df['daily_population_difference'] = daily_population_df.apply(lambda x: (x.day_of_year/daily_population_df[daily_population_df['year'] == x.year].shape[0]) * (yearly_df[yearly_df['year'] == x.year + 1].iloc[0].population - x.population), axis=1)
     daily_population_df['total_daily_population'] = daily_population_df['daily_population_difference'] + daily_population_df['population']
-    print('bp')
-    return daily_population_df.to_csv('daily_population.csv', sep=',', index=False)
+    return daily_population_df.to_csv('../data/daily_population.csv', sep=',', index=False)
 
 if __name__ == "__main__":
     test = daily_population_csv(pd.read_csv('../raw_data/nyc_population.csv', sep='\t'))
-    print('bp')
-
